d5cf23aa7b48a93bcf01304180a00736803bb300
Alex Dițu <ditu.alexandru@gmail.com>
2021-06-01 16:45:02 +0300

    build.sh: setup custom toolchain (gcc-10.2.0)

--- build.sh
+++ build.sh
@@ -11,8 +11,8 @@ rm -rf "${BUILD_DIR}"
 mkdir -p "${BUILD_DIR}"
 cd "${BUILD_DIR}" || (echo "cd to ${BUILD_DIR} failed"; exit)
 
-cmake .. -DCMAKE_INSTALL_PREFIX=install
-cmake --build . --target install
-cmake --build . --target package
+cmake .. -DCMAKE_INSTALL_PREFIX=install -DCMAKE_TOOLCHAIN_FILE=../toolchain.cmake
+cmake --build . --parallel --target install
+cmake --build . --parallel --target package
 
 sha256sum "third_party_cpp_deps.tar.gz"
